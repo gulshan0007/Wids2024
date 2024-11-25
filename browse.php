32a767de17c82f2633010867761f2acf9cffda2c
gulshan0007 <121476861+gulshan0007@users.noreply.github.com>
2024-11-25 23:06:08 +0530

    Update browse.php

--- browse.php
+++ browse.php
@@ -158,24 +158,7 @@ opacity: 0.85; color:white">
         <li class="nav-item">
           <a class="nav-link" href="team.php">Contact Us</a>
         </li>
-        <?php if (!isset($_SESSION['ldap'])) { ?>
-		            <li class="nav-item">
-		                <a class="nav-link"
-		                    href="https://sso.tech-iitb.org/project/7300a264-d142-49df-93f5-81494a6fdf62/ssocall/"
-		                    target="_parent">Login via SSO</a>
-		            </li>
-		            <?php } else { ?>
-                        <li class="nav-item">
-                        <a class="nav-link"  href="https://docs.google.com/forms/d/e/1FAIpQLSeDU_ovmdwf4hRA68cMZaQGGpEGPIl9tFWDXGwSeUN7-kqKtQ/viewform" target="_blank">Apply Now</a>
-                    </li>
-		            <li class="nav-item">
-		                <form method="POST" action="logout.php">
-		                    <button type="submit" class="btn btn-dark mx-3 my-md-0  dotted-border"
-		                        name="Logout" style="margin-top: 2px; margin-left: 10px; width: 100px; height: 40px;">
-		                        LogOut <i class="fas fa-sign-out-alt"></i></button>
-		                </form>
-		            </li>
-		            <?php } ?>
+        
       </ul>
     </div>
 
@@ -261,7 +244,7 @@ $categories = [
                 <h5 class="mb-3">Contact us <i class="fa fa-phone"></i></h5>
                 <ul class="list-unstyled text-muted">
                     <li class="small text-muted">
-                        <strong style="color: #11163d; font-size: 1.25em">Aansh Samyani
+                        <strong style="color: #11163d; font-size: 1.25em">Ansh Samyani
                         </strong>
                     </li>
                     <li class="small text-muted">
